a88dadc127b4a8c87dc5ed65d87aeac58eb52123
HectorLopez1998 <hectorlopezp1998@gmail.com>
2020-11-11 10:55:48 -0500

    commit tree 4e1bc0e859903d3f3bf673c8b0b07acefc1bbe3d

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,10 @@
 def square_array(array)
-  # your code here
+  arr = []
+  array.each { |i| arr << i ** 2 }
+  arr
 end
+
+#  array = [1,2,3]
+#  array.each do |index|
+#      puts index ** 2
+# end
